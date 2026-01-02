b4df341ffb0de15f720e8eeb34825d000ad732ff
giovannepe-dev <giovannepe@gmail.com>
2026-01-01 23:12:17 -0300

    Delete pagina-pessoal/pagina-pessoal directory

--- pagina-pessoal/pagina-pessoal/script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById("btnMensagem");
-const mensagem = document.getElementById("mensagem");
-
-botao.addEventListener("click", () => {
-    mensagem.textContent = "Obrigado por visitar meu projeto!";
-});
